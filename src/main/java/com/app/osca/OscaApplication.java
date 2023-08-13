e89c6d18cd8b3060da1bce85a2653fac951ba8c6
devyujinjeong <devyujinjeong887@gmail.com>
2023-08-14 00:55:22 +0900

    no message

---
+++ src/main/java/com/app/osca/OscaApplication.java
@@ -0,0 +1,13 @@
+package com.app.osca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OscaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OscaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/osca/OscaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.osca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OscaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
