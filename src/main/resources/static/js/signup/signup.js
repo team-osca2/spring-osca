af70d2249ecf581a5385499176a57baf1765e883
Dev-RuntimeTerror <dev.uheejoon@gmail.com>
2023-08-19 23:04:42 +0900

    경로 및 js 수정 _ 유희준

--- src/main/resources/static/js/signup/signup.js
+++ src/main/resources/static/js/signup/signup.js
@@ -1,40 +1,69 @@
 
+// email
 const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-const email = document.querySelector('input[name="email"]');
-const emailErrorMessage = document.querySelector(".email-input-area .input-error-message");
-email.addEventListener("input", event => emailCheck(event.target));
+const $email = $('.input[name="email"]');
+const $emailErrorMessageBox = $('.email-input-area .input-error-message');
+const emailErrorMessage = '이메일 형식이 유효하지 않습니다.';
+$email.on('input', validationEmailOrPassword(checkValidation)($emailErrorMessageBox, emailErrorMessage, emailRegex));
 
 
-
+// password
 const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/;
-const password = document.querySelector('input[name="password"]');
-const passwordErrorMessage = document.querySelector(".password-input-area .input-error-message");
-password.addEventListener("input", event => passwordCheck(event.target));
+const $password = $('input[name="password"]');
+const $passwordErrorMessageBox = $(".password-input-area .input-error-message");
+const passwordErrorMessage = '8자 이상, 영문 대 소문자, 숫자, 특수문자를 사용하세요.';
+$password.on('input', validationEmailOrPassword(checkValidation)($passwordErrorMessageBox, passwordErrorMessage, passwordRegex));
+
+
+// passwordConfirm
+const $passwordConfirm = $('input[name="passwordConfirm"]');
+const $passwordConfirmErrorMessageBox = $(".password-confirm-input-area .input-error-message");
+const passwordConfrimErrorMessage = '비밀번호를 확인해 주세요.';
+$passwordConfirm.on('input', validationEmailOrPassword(passwordConfirm)($passwordConfirmErrorMessageBox, passwordConfrimErrorMessage));
+
+
+Object.prototype.forEach = Array.prototype.forEach;
+const $selectMenuList = $(".input-select");
+const $selectMenuContentsList = $(".select-menu");
+
+$selectMenuList.forEach((selectMenu, idx) => $(selectMenu).on('click', expandSelectMenu(idx)));
+
+$selectMenuContentsList.forEach((selectMenuContent, idx) => {
+    const $content = $(selectMenuContent).find('.select-menu-option');
+    const $inputArea = $(selectMenuContent).siblings().find('.selected-content-box');
+    $content.on('click', function(){
+        if(idx < 2){
+            $content.removeClass('selected-menu');
+            $(this).addClass('selected-menu');
+            $inputArea.css('color', 'rgb(48, 52, 65)');
+            $inputArea.text($(this).text().trim());
+            expandSelectMenu(idx)();
+        }
+        else {
+            let count = 0;
+            $(this).toggleClass('selected-menu');
+            $content.forEach((el) => count += $(el).hasClass('selected-menu'));
+            $inputArea.text(count == 0 ? '관심사를 선택해 주세요.' : count);
+        }
+    });
+});
+
+const $submitBtn = $('button[type="button"]');
+// const submitButton = document.querySelector('button[type="button"]');
+// submitButton.addEventListener("click", submitButtonClick);
 
 
 
-const passwordConfirm = document.querySelector('input[name="passwordConfirm"]');
-const passwordConfirmErrorMessage = document.querySelector(".password-confirm-input-area .input-error-message");
-passwordConfirm.addEventListener("input", event => passwordConfirmCheck(event.target));
+// const inputTags = document.querySelectorAll("input");
+// inputTags.forEach(inputTag => inputTag.addEventListener("keydown", blockEnter));
 
 
 
-const selectMenuList = document.querySelectorAll(".input-select-body");
-const selectMenuContentsList = document.querySelectorAll(".select-menu");
-selectMenuList.forEach((selectMenu, idx) => selectMenu.onclick = event => expandSelectMenu(event.target.children[0], idx));
+// const checkItemList = [email, password, passwordConfirm];
 
 
-
-const submitButton = document.querySelector('button[type="button"]');
-submitButton.addEventListener("click", submitButtonClick);
-
-
-
-const inputTags = document.querySelectorAll("input");
-inputTags.forEach(inputTag => inputTag.addEventListener("keydown", blockEnter));
-
-
-
-const checkItemList = [email, password, passwordConfirm];
-
-
+// checkBox
+const $checkAll = $('.checkbox-checkall-box');
+$checkAll.on('click', allCheckBoxToggleClass);
+const $checkList = $('.checkbox-svg-wrapper');
+$checkList.on('click', checkBoxToggle);
--- src/main/resources/static/js/signup/signup_func.js
+++ src/main/resources/static/js/signup/signup_func.js
@@ -1,77 +1,42 @@
 
-// email 유효성 검사
-function emailCheck(target){
-    let contents = target.value;
-    if(!contents.match(emailRegex)){
-        emailErrorMessage.innerText = '이메일 형식이 유효하지 않습니다.';
-        return false;
-    }
-    emailErrorMessage.innerText = '';
-    return true;
+/** 유효성 검사 조건 : 조건식, 에러 메시지 출력 위치, 에러 메시지 순서로 변수 들어옴*/
+const returnValidationStatementAndChangeMessage = (state) => (errorBox, errorMessage) => {
+    errorBox.text(state ? '' : errorMessage);
+    return state;
 }
 
-// password 유효성 검사
-function passwordCheck(target){
-    let contents = target.value;
-    if(!contents.match(passwordRegex)){
-        passwordErrorMessage.innerText = '8자 이상, 영문 대 소문자, 숫자, 특수문자를 사용하세요.';
-        return false;
-    }
-    passwordErrorMessage.innerText = '';
-    return true;
+/** 비밀번호 확인 */
+const passwordConfirm = (errorBox, errorMessage, e) => returnValidationStatementAndChangeMessage(e.target.value === $password.val()) (errorBox, errorMessage)
+
+/** email, password 유효성 검사 */
+const checkValidation = (errorBox, errorMessage, e, regex) => returnValidationStatementAndChangeMessage((e.target.value).match(regex)) (errorBox, errorMessage)
+
+/** 타입별 유효성 검사 */
+const validationEmailOrPassword = (validationCallback) => (errorBox, errorMessage, regex) => (e) => validationCallback(errorBox, errorMessage, e, regex);
+
+/** select 박스 열고 닫기 */
+const expandSelectMenu = (idx) => ()=> $($selectMenuContentsList[idx]).toggle();
+
+/** 버튼 클릭 이벤트 */
+// const checkFuncList = [emailCheck, passwordCheck, passwordConfirmCheck];
+// function submitButtonClick(e){
+//     checkFuncList.forEach((checkFunc, idx) => {
+//         let item = checkItemList[idx];
+//         if(!checkFunc(item)){
+//             item.focus();
+//             return;
+//         }
+//     });
+
+//     document.getElementById("signup_form").submit();
+// }
+
+function allCheckBoxToggleClass(e){
+    $(this).toggleClass('selected-checkbox');
+    const checkListClass = 'checkbox-svg-wrapper ' + ($(this).hasClass('selected-checkbox') ? 'selected-checkbox' : '');
+    $checkList.forEach((check) => $(check).attr('class', checkListClass));
 }
 
-// password 확인
-function passwordConfirmCheck(target){
-    let contents = target.value;
-    let password_value = password.value;
-    if(contents !== password_value){
-        passwordConfirmErrorMessage.innerText = '비밀번호를 확인해 주세요.';
-        return false;
-    }
-    passwordConfirmErrorMessage.innerText = '';
-    return true;
-}
-
-// select 박스 열고 닫기
-let index = 0;
-function expandSelectMenu(selectMenu, idx){
-    index = idx;
-    let currentMenuStatus = selectMenuContentsList[index].style.display;
-    if(currentMenuStatus == 'none'){
-        selectMenuContentsList[index].style.display = 'block';  
-        selectMenuContentsList[index].addEventListener('click', bodyClickCloseSelectMenu);
-    }
-    else{
-        // selectMenuContentsList[index].style.display = 'none';
-        selectMenuContentsList[index].removeEventListener('click', bodyClickCloseSelectMenu);
-    }
-}
-
-function bodyClickCloseSelectMenu(e){
-    console.log(e.target)
-    if(!selectMenuList[index].contains(e.target)){
-        selectMenuContentsList.forEach(selectMenuContent => selectMenuContent.style.display = 'none');
-    }
-}
-
-// input태그에서 enter키 막기
-function blockEnter(enter){
-    if(enter.code === "Enter"){
-        enter.preventDefault();
-    }
-}
-
-// 버튼 클릭 이벤트
-const checkFuncList = [emailCheck, passwordCheck, passwordConfirmCheck];
-function submitButtonClick(e){
-    checkFuncList.forEach((checkFunc, idx) => {
-        let item = checkItemList[idx];
-        if(!checkFunc(item)){
-            item.focus();
-            return;
-        }
-    });
-
-    document.getElementById("signup_form").submit();
+function checkBoxToggle(e){
+    $(this).toggleClass('selected-checkbox');
 }
