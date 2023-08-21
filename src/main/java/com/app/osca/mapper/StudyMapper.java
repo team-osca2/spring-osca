d1cb44a54858bfd3f374efa684edb7f998418880
Dev-RuntimeTerror <dev.uheejoon@gmail.com>
2023-08-21 18:14:52 +0900

    studyMapper 추가햣

--- src/main/java/com/app/osca/mapper/StudyMapper.java
+++ src/main/java/com/app/osca/mapper/StudyMapper.java
@@ -1,7 +1,9 @@
 package com.app.osca.mapper;
 
+import com.app.osca.domain.StudyVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface StudyMapper {
+    public void insert(StudyVO studyVO);
 }
