77b0e2fdc0ac2f0f474213431a6c68d83ec85027
Dev-RuntimeTerror <dev.uheejoon@gmail.com>
2023-08-25 00:42:23 +0900

    cafeAdDetailDTO 가져오기

--- src/main/java/com/app/osca/dao/CafeAdDAO.java
+++ src/main/java/com/app/osca/dao/CafeAdDAO.java
@@ -1,11 +1,20 @@
 package com.app.osca.dao;
 
+import com.app.osca.domain.dto.cafeAd.CafeAdDetailDTO;
 import com.app.osca.mapper.CafeAdMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class CafeAdDAO {
     private final CafeAdMapper cafeAdMapper;
+
+    public Optional<CafeAdDetailDTO> findByID(Long id){
+        return cafeAdMapper.select(id);
+    }
 }
--- src/main/java/com/app/osca/mapper/CafeAdMapper.java
+++ src/main/java/com/app/osca/mapper/CafeAdMapper.java
@@ -1,8 +1,14 @@
 package com.app.osca.mapper;
 
+import com.app.osca.domain.dto.cafeAd.CafeAdDetailDTO;
+import com.app.osca.domain.paging.Criteria;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface CafeAdMapper {
+    public void selectAll(Criteria criteria);
 
+    public Optional<CafeAdDetailDTO> select(Long id);
 }
