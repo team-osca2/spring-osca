1a2e4d49cdf527b6ec3319630016d7b628e7b600
Dev-RuntimeTerror <dev.uheejoon@gmail.com>
2023-08-25 20:12:14 +0900

    테스트 코드 수정 및 추가

--- src/test/java/com/app/osca/cafe/controller/CafeControllerTest.java
+++ src/test/java/com/app/osca/cafe/controller/CafeControllerTest.java
@@ -1,4 +1,4 @@
-package com.app.osca.controller.cafe;
+package com.app.osca.cafe.controller;
 
 import com.app.osca.domain.dto.cafeAd.CafeAdDetailDTO;
 import lombok.extern.slf4j.Slf4j;
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Objects;
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Slf4j
-public class CafeControllerTest {
+public class CafeControllerTest{
     @Autowired
     private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
@@ -26,6 +26,18 @@ public class CafeControllerTest {
     @BeforeEach
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void goToMyCafeListTest() throws Exception {
+        assertThat(
+            Objects.requireNonNull(mockMvc.perform(MockMvcRequestBuilders.get("/cafe/my-cafe"))
+                    .andReturn()
+                    .getModelAndView()
+            )
+            .getModelMap()
+            .get("myCafeList")
+        ).isEqualTo("sdf");
     }
 
     @Test
--- src/test/java/com/app/osca/cafe/dao/CafeDAOTest.java
+++ src/test/java/com/app/osca/cafe/dao/CafeDAOTest.java
@@ -1,4 +1,4 @@
-package com.app.osca.dao.cafe;
+package com.app.osca.cafe.dao;
 
 import com.app.osca.dao.CafeDAO;
 import com.app.osca.domain.CafeVO;
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Slf4j
@@ -19,4 +21,8 @@ public class CafeDAOTest {
         cafeDAO.findAll().stream().map(CafeVO::toString).forEach(log::info);
     }
 
+    @Test
+    public void findByIdTest(){
+        assertThat(cafeDAO.findById(2L).orElseGet(CafeVO::new).getCafeName()).isEqualTo("카페베네");
+    }
 }
