e8da6eee13532dfe86af3a0eef755578053dfbc6
AlainaKIM <console.log.hyebin@gmail.com>
2023-08-26 11:20:54 +0900

    study mapper 추가

--- src/main/java/com/app/osca/mapper/StudyMapper.java
+++ src/main/java/com/app/osca/mapper/StudyMapper.java
@@ -2,10 +2,16 @@ package com.app.osca.mapper;
 
 import com.app.osca.domain.StudyVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface StudyMapper {
     public void insert(StudyVO studyVO);
+
+    //    스터디 목록
+//    public List<StudyVO> selectStudies(@Param("pagination")Pagination pagination, @Param("search")Search search);
 
     //    관리자 스터디 게시글 불러오기
 //    public List<studyDTO> selectAllStudyPost(Long studyId);
