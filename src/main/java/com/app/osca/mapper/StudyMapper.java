106c1411098b686298109c06a429dfae3ba32a04
AlainaKIM <console.log.hyebin@gmail.com>
2023-08-26 14:23:55 +0900

    studyMapper 다시 추가

--- src/main/java/com/app/osca/mapper/StudyMapper.java
+++ src/main/java/com/app/osca/mapper/StudyMapper.java
@@ -2,9 +2,13 @@ package com.app.osca.mapper;
 
 import com.app.osca.domain.StudyVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface StudyMapper {
     public void insert(StudyVO studyVO);
 
+//    public List<StudyVO> selectStudies(@Param("pagination")Pagination pagination, @Param("search")Search search);
+
+    
 }
